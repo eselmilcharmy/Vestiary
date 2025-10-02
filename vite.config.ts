069bee497e8066ea3e1ef36ad37d5e0d95beaee2
eselmilcharmy <albinot.hajrizaj@stud.hs-kempten.de>
2025-10-02 11:20:25 +0200

    back to normal

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Vestiary/", // <<< WICHTIG für GitHub Pages!
   server: {
     host: "::",
     port: 8080,
